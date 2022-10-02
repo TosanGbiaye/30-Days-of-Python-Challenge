1ed9b98e38deba21b89a60e2f59f3be950097819
TosanGbiaye <36245819+TosanGbiaye@users.noreply.github.com>
2022-10-02 14:15:36 +0100

    Update the variables file

--- Day 2 Challenge/Variables.py
+++ Day 2 Challenge/Variables.py
@@ -5,4 +5,16 @@ fullName = firstName + ' ' + lastName
 print(fullName)
 country = "Nigeria"
 city = "Lagos"
-Print(city, country)
+print(city, country)
+age = 25
+year = 2022
+is_married = False
+is_true = "Yes"
+is_light_on = "Yes"
+first_name, last_name, country, age, is_married = 'Tosan', 'Gbiaye', 'Nigeria', 25, 'Yes'
+print(first_name, last_name, country, age, is_married)
+print('First name:', first_name)
+print('Last name: ', last_name)
+print('Country: ', country)
+print('Age: ', age)
+print('Married: ', is_married)
