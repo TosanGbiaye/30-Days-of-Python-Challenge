fa67d473cabdd3efc19de104e056e9150391b4c9
TosanGbiaye <36245819+TosanGbiaye@users.noreply.github.com>
2022-10-02 13:56:35 +0100

    Create Variables.py
    Update the new file

---
+++ Day 2 Challenge/Variables.py
@@ -0,0 +1,8 @@
+print("30 Days of Python Programming")
+firstName = "Tosan"
+lastName = "Gbiaye"
+fullName = firstName + ' ' + lastName
+print(fullName)
+country = Nigeria
+city = Lagos
+Print(city, country)
