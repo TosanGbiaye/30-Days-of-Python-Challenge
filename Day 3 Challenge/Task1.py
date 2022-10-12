bd68f9920c96bd42675b369f2f810df82e3028d7
TosanGbiaye <36245819+TosanGbiaye@users.noreply.github.com>
2022-10-12 20:29:43 +0100

    Update file

--- Day 3 Challenge/Task1.py
+++ Day 3 Challenge/Task1.py
@@ -1,5 +1,24 @@
+# Declare your age as integer variable
+# Declare your height as a float variable
+# Declare a variable that store a complex number
+
 my_age = 25
 my_height = 5.2
 complex_number = 1 +1j
-print("Age:",my_age, "Height:",my_height,"Complex number:",complex_number")
+print("Age:",my_age, "Height:",my_height,"Complex number:",complex_number)
+
+# 4. Write a script that prompts the user to enter base and height of the triangle and calculate an area of this triangle (area = 0.5 x b x h).
+b = int(input("Enter base: "))
+h = int(input("Enter height: "))
+area_of_triangle = 0.5 * b * h
+print("Area:",area_of_triangle)
+
+# 5. Write a script that prompts the user to enter side a, side b, and side c of the triangle. Calculate the perimeter of the triangle (perimeter = a + b + c).
+a = int(input("Enter side a: "))
+b = int(input("Enter side b: "))
+c = int(input("Enter side c: "))
+perimeter_of_triangle = a + b + c
+print("Perimeter: ",perimeter_of_triangle)
+
+
       
